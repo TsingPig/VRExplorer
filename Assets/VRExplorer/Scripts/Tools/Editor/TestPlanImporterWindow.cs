3c3081aad6b1f39e60da93d04fb4f4d62846b692
tsingpig <1114196607@qq.com>
2025-08-15 22:09:52 +0800

    upd: Print Object FileID function in TestPlanImporter.cs

--- Assets/VRExplorer/Scripts/Tools/Editor/TestPlanImporterWindow.cs
+++ Assets/VRExplorer/Scripts/Tools/Editor/TestPlanImporterWindow.cs
@@ -1,6 +1,8 @@
 #if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
+using UnityEditor.SceneManagement;
+using System;
 
 namespace VRExplorer
 {
@@ -19,7 +21,7 @@ namespace VRExplorer
         {
             GUILayout.Label("Test Plan Importer", EditorStyles.boldLabel);
 
-            //物体选择器
+            // 物体选择器
             selectedObject = (GameObject)EditorGUILayout.ObjectField("Select Object", selectedObject, typeof(GameObject), true);
 
             // 打印GUID按钮
@@ -31,6 +33,27 @@ namespace VRExplorer
                 ShowNotification(new GUIContent($"GUID copied to clipboard: {guid}"));
             }
 
+            if(GUILayout.Button("Print Object FileID") && selectedObject != null)
+            {
+                try
+                {
+                    long fileId = GetSceneObjectFileID(selectedObject);
+                    if(fileId != 0)
+                    {
+                        Debug.Log($"FileID for {selectedObject.name}: {fileId}");
+                        EditorGUIUtility.systemCopyBuffer = fileId.ToString();
+                        ShowNotification(new GUIContent($"FileID copied to clipboard: {fileId}"));
+                    }
+                    else
+                    {
+                        Debug.LogError($"Failed to get FileID for {selectedObject.name}. Is it a scene object?");
+                    }
+                }
+                catch(Exception e)
+                {
+                    Debug.LogError($"Failed to get FileID: {e.Message}");
+                }
+            }
             GUILayout.Space(20);
 
             // TestPlan文件路径选择
@@ -48,6 +71,39 @@ namespace VRExplorer
                 VREscaper.ImportTestPlan(filePath);
             }
         }
+
+        private long GetSceneObjectFileID(GameObject go)
+        {
+            if(go == null) return 0;
+
+            var scenePath = go.scene.path;
+            if(string.IsNullOrEmpty(scenePath))
+            {
+                Debug.LogError("Scene not saved to disk!");
+                return 0;
+            }
+
+            var lines = System.IO.File.ReadAllLines(scenePath);
+            for(int i = 0; i < lines.Length; i++)
+            {
+                if(lines[i].Contains("m_Name: " + go.name))
+                {
+                    // 往上查找 "--- !u!1 &<fileID>"
+                    for(int j = i; j >= 0; j--)
+                    {
+                        if(lines[j].StartsWith("--- !u!1 &"))
+                        {
+                            string fileIDStr = lines[j].Substring("--- !u!1 &".Length);
+                            if(long.TryParse(fileIDStr, out long fileID))
+                                return fileID;
+                        }
+                    }
+                }
+            }
+
+            return 0;
+        }
+
     }
 }
 #endif
